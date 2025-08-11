a765de2045fdac61392f4c6f927577abd398b101
btbuilds <bthompson8807@gmail.com>
2025-08-11 08:26:41 -0400

    Added regex checks for markdown images and links with associated tests.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+import re
 
 def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: TextType):
     new_nodes = []
@@ -17,3 +18,11 @@ def split_nodes_delimiter(old_nodes: list[TextNode], delimiter: str, text_type: 
                 else:
                     new_nodes.append(TextNode(part, text_type))
     return new_nodes
+
+def extract_markdown_images(text):
+    matches = re.findall(r"\!\[(.*?)\]\((.*?)\)", text)
+    return matches
+
+def extract_markdown_links(text):
+    matches = re.findall(r"\[(.*?)\]\((.*?)\)", text)
+    return matches
---
+++ src/test_regex_images_and_links.py
@@ -0,0 +1,58 @@
+import unittest
+from inline_markdown import extract_markdown_images, extract_markdown_links
+
+
+class TestExtractMarkdownImages(unittest.TestCase):
+    def test_single_image(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual(
+            [("image", "https://i.imgur.com/zjjcJKZ.png")], matches
+        )
+
+    def test_multiple_images(self):
+        matches = extract_markdown_images(
+            "![first](http://example.com/1.png) some text ![second](http://example.com/2.png)"
+        )
+        self.assertListEqual(
+            [
+                ("first", "http://example.com/1.png"),
+                ("second", "http://example.com/2.png"),
+            ],
+            matches,
+        )
+
+    def test_no_images(self):
+        matches = extract_markdown_images("This text has no images at all.")
+        self.assertListEqual([], matches)
+
+
+class TestExtractMarkdownLinks(unittest.TestCase):
+    def test_single_link(self):
+        matches = extract_markdown_links(
+            "Here is a [link](https://example.com) in text."
+        )
+        self.assertListEqual(
+            [("link", "https://example.com")], matches
+        )
+
+    def test_multiple_links(self):
+        matches = extract_markdown_links(
+            "[first](http://example.com/1) and [second](http://example.com/2)"
+        )
+        self.assertListEqual(
+            [
+                ("first", "http://example.com/1"),
+                ("second", "http://example.com/2"),
+            ],
+            matches,
+        )
+
+    def test_no_links(self):
+        matches = extract_markdown_links("No markdown links here.")
+        self.assertListEqual([], matches)
+
+
+if __name__ == "__main__":
+    unittest.main()
